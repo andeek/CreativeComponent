cf5283a2f8928bdb34fd61711db6c42691fdb377
andeek <andee.kaplan@gmail.com>
2014-03-20 11:48:04 -0500

    remove padding

--- presentation/scripts/charts.js
+++ presentation/scripts/charts.js
@@ -3,8 +3,7 @@ function particleChart() {
     var i = 0;
     
     var width = 1280,
-        height = 720,
-        padding = 50;
+        height = 720;
     
     
     var svg = d3.select(".particle-chart").append("svg")
@@ -22,8 +21,8 @@ function particleChart() {
         var m = d3.mouse(this);
     
         svg.insert("circle", "rect")
-            .attr("cx", m[0] + padding)
-            .attr("cy", m[1] + padding)
+            .attr("cx", m[0])
+            .attr("cy", m[1])
             .attr("r", 1e-6)
             .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
             .style("stroke-opacity", 1)
